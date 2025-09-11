75faef54a2c6bfd50bd87574b59f7806988d4ed1
VikasPrasad27 <vjp66778899@gmail.com>
2025-09-11 22:44:53 +0530

    Count Char without White Spaces

--- SetCountChar.cpp
+++ SetCountChar.cpp
@@ -1,22 +1,38 @@
-//count char in a string & remove duplicates if there
-//problem in someones placement round
+// //count char in a string & remove duplicates if there
+// //problem in someones placement round
 
-#include<iostream>
-#include<set>
+// #include<iostream>
+// #include<set>
+// using namespace std;
+
+// int main(){
+//     string name="vikasprasad";
+//     int count=0;
+//     set<char> st;
+
+//     for(int i=0;i<name.length();i++){
+//         count++;
+//         st.insert(name[i]);
+//     }
+//     cout<<count;
+//     for(auto it:st){
+//         cout<<it;
+//     }
+//     return 0;
+// 
+
+// Count Char without whitespaces
+#include <iostream>
 using namespace std;
 
 int main(){
-    string name="vikasprasad";
-    int count=0;
-    set<char> st;
-
-    for(int i=0;i<name.length();i++){
-        count++;
-        st.insert(name[i]);
+    string name="Hello World";
+    int cnt=0;
+    for(auto ch:name){
+        if(ch != ' '){
+            cnt++;
+        }
     }
-    cout<<count;
-    for(auto it:st){
-        cout<<it;
-    }
+    cout<<cnt;
     return 0;
 }
