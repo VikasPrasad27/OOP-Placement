64473e8e135e9e1e183eb635f060ff96c2746061
VikasPrasad27 <vjp66778899@gmail.com>
2025-10-10 21:16:23 +0530

    added non recurive fibonacci code

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,20 +1,42 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// int fib(int n){
+//     if(n<=1){
+//         return n;
+//     }
+//     return fib(n-1)+fib(n-2);
+// }
+
+// int main(){
+//     int n;
+//     cout<<"ENTER INDEX TILL WHICH TO GENERATE:";
+//     n=6;
+
+//     for(int i=0;i<n;i++){
+//         cout<<fib(i);
+//     }
+//     return 0;
+// }
+
+
+//NON RECURSIVE - ITERATIVE
+#include<iostream>
 using namespace std;
 
-int fib(int n){
-    if(n<=1){
-        return n;
+void fib(int n){
+    int a=0,b=1,next;
+    for(int i=0;i<=n;i++){
+        cout<<a<<" ";
+        next=a+b;
+        a=b;
+        b=next;
     }
-    return fib(n-1)+fib(n-2);
 }
-
 int main(){
+    cout<<"ENTER NUMBER TILL WHICH TO GENERATE";
     int n;
-    cout<<"ENTER INDEX TILL WHICH TO GENERATE:";
     n=6;
-
-    for(int i=0;i<n;i++){
-        cout<<fib(i);
-    }
+    fib(n);
     return 0;
 }
