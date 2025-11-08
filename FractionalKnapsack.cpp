983ca52c3fbfef9042537dce964734603df0347b
VikasPrasad27 <vjp66778899@gmail.com>
2025-11-08 18:21:05 +0530

    Fractional Knapsack using Greedy Method

---
+++ FractionalKnapsack.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item { double value, weight; };
+bool cmp(Item a, Item b) { return a.value / a.weight > b.value / b.weight; }
+
+int main() {
+    int n;
+    double W, maxValue = 0;
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    vector<Item> items(n);
+    cout << "Enter value and weight of each item:\n";
+    for (int i = 0; i < n; i++)
+        cin >> items[i].value >> items[i].weight;
+
+    cout << "Enter capacity of knapsack: ";
+    cin >> W;
+
+    sort(items.begin(), items.end(), cmp);
+
+    for (auto &it : items) {
+        if (it.weight <= W) {  // take whole item
+            maxValue += it.value;
+            W -= it.weight;
+        } else {                // take fraction
+            maxValue += it.value * (W / it.weight);
+            break;
+        }
+    }
+
+    cout << "Maximum value in knapsack = " << maxValue;
+}
