5429226c15e141dfaba3e3f166a7b5888a72f8ae
VikasPrasad27 <vjp66778899@gmail.com>
2026-03-13 20:31:07 +0530

    TCS arrangement question

---
+++ TCS_Coding/Arrangement.cpp
@@ -0,0 +1,44 @@
+/*
+An international round table conference will be held in india. Presidents from all over the world representing their respective countries will be attending the conference. The task is to find the possible number of ways(P) to make the N members sit around the circular table such that.
+
+The president and prime minister of India will always sit next to each other.
+
+Example 1:
+
+Input :
+
+4   -> Value of N(No. of members)
+
+Output : 
+
+12  -> Possible ways of seating the members
+
+Explanation:
+
+2  members should always be next to each other. 
+
+So, 2 members can be in 2!ways
+
+Rest of the members can be arranged in (4-1)! ways.(1 is subtracted because the previously selected two members will be considered as single members now).
+
+So total possible ways 4 members can be seated around the circular table 2*6= 12.
+
+Hence, output is 12
+*/
+
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main(){
+    int n;
+    cout<<"Enter N";
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
