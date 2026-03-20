9be6ca92b5db4b9a5858bbb666f9d9af0316e54b
VikasPrasad27 <vjp66778899@gmail.com>
2026-03-20 08:57:48 +0530

    String NQT Problem Alternate way

--- TCS_Coding/q3.cpp
+++ TCS_Coding/q3.cpp
@@ -34,3 +34,31 @@ int main(){
     }
     return 0;
 }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string keywords[16] = {"break","case","continue","default","defer","else",
+                          "for","func","goto","if","map","range",
+                          "return","struct","type","var"};
+    
+    string input;
+    cin >> input;
+
+    bool isKeyword = false;
+
+    for(int i = 0; i < 16; i++){
+        if(input == keywords[i]){
+            isKeyword = true;
+            break;
+        }
+    }
+
+    if(isKeyword)
+        cout << input << " is a keyword";
+    else
+        cout << input << " is not a keyword";
+
+    return 0;
+}
