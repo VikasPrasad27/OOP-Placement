f4ff2a60a796c9476a265572ff3332c9ddd6f3e6
VikasPrasad27 <vjp66778899@gmail.com>
2026-03-20 09:01:57 +0530

    Tcs

--- TCS_Coding/q4.cpp
+++ TCS_Coding/q4.cpp
@@ -44,3 +44,31 @@ int main(){
     }
     return 0;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    int j = 0;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] != 0){
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    for(int x : arr){
+        cout << x << " ";
+    }
+
+    return 0;
+}
