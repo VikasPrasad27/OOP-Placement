3738e33e44980affd7fe2b916a6c33d07ba4db0b
VikasPrasad27 <vjp66778899@gmail.com>
2026-05-03 20:43:42 +0530

    Parallel reduction using OpenMP

---
+++ HPC using OpenMP/parallel_reduction.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int arr[] = {1, 2, 3, 4, 5};
+
+    int minv = arr[0], maxv = arr[0], sum = 0;
+
+    #pragma omp parallel for reduction(min:minv) reduction(max:maxv) reduction(+:sum)
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < minv) minv = arr[i];
+        if (arr[i] > maxv) maxv = arr[i];
+        sum += arr[i];
+    }
+
+    double avg = (double)sum / n;
+
+    cout << "Min: " << minv << endl;
+    cout << "Max: " << maxv << endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Avg: " << avg << endl;
+
+    return 0;
+}
